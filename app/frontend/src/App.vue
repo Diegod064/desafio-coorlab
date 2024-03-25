dd53aebb4b666485a3e299345dbd8738a41ff3a5
Diego Diniz <diegodcg064@gmail.com>
2024-03-24 23:35:35 -0300

    creating modal

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -9,15 +9,28 @@ const destination = ref('');
 const date = ref('');
 const cities = ref<string[]>([]);
 const noDataSelected = ref(true);
+const searchResults = ref([]);
+const dialog = ref(false)
 
 const handleSearch = () => {
   if (!destination.value || !date.value) {
     console.log("Por favor, preencha todos os campos");
     noDataSelected.value = true;
+    dialog.value = true;
+    console.log(dialog.value)
   } else {
     console.log(`Estas são as melhores alternativas de viagem para a data selecionada ${destination.value} ${date.value}`);
     noDataSelected.value = false;
+    searchTravels();
+    console.log(searchResults.value)
   }
+}
+
+const handleClear = () => {
+  noDataSelected.value = true;
+  searchResults.value = []
+  destination.value = '';
+  date.value = '';
 }
 
 const sampleObj = {
@@ -47,7 +60,7 @@ const searchTravels = () => {
       })
       .then(data => {
         resolve(data);
-        console.log(data)
+        searchResults.value = data;
       })
       .catch(error => {
         reject(error);
@@ -71,6 +84,7 @@ onMounted(() => {
 });
 
 
+
 </script>
 
 
@@ -84,32 +98,37 @@ onMounted(() => {
         <v-icon icon="mdi-truck-delivery-outline" />
         <span>Calculadora de Viagem</span>
       </div>
+      <v-dialog v-model="dialog" width="auto">
+              <div class="modal">
+                <v-icon>mdi-exclamation-thick</v-icon>
+                <p>Insira os valores para realizar a cotação</p>
+                <v-btn @click="dialog = false">Fechar</v-btn>
+              </div>
+            </v-dialog>
 
       <div class="content-container">
         <div class="left-container">
-          <div class="title">
-            <v-icon icon="mdi-hand-coin-outline"></v-icon>
-            <h3>Calcule o Valor da Viagem</h3>
-          </div>
-          <!-- Inputs -->
-          <div class="inputs">
-            <div class="input-group">
-              <label for="destino">Destino:</label>
-              <!-- <input type="text" id="destino"> -->
-              <v-select label="Select" :items="cities"
-                variant="outlined" v-model="destination"></v-select>
+          <div class="left-container-content">
+            <div class="title">
+              <v-icon icon="mdi-hand-coin-outline"></v-icon>
+              <h3>Calcule o Valor da Viagem</h3>
             </div>
-            <div class="input-group">
-              <label for="data">Data:</label>
-              <input label="asdasd" type="date" id="data" v-model="date">
+            <div class="inputs">
+              <div class="input-group">
+                <label for="destino">Destino:</label>
+                <v-select label="select" :items="cities" variant="outlined" v-model="destination"></v-select>
+              </div>
+              <div class="input-group">
+                <label for="data">Data:</label>
+                <input label="asdasd" type="date" id="data" v-model="date">
 
+              </div>
             </div>
+            <button class="search-btn" @click="handleSearch">Buscar</button>
           </div>
 
-          <button class="buscar-btn" @click="searchTravels">Buscar</button>
         </div>
 
-        <!-- Container da direita (será implementado posteriormente) -->
         <div class="right-container">
           <div class="right-container-content" v-if="noDataSelected">
             <h3>Não há nenhum dado seleciondo</h3>
@@ -117,7 +136,11 @@ onMounted(() => {
 
           <div class="right-container-content" v-else>
             <h3>Estas são as melhores alternativas de viagem para a data selecionada</h3>
-            <TravelOptionItem :option=sampleObj></TravelOptionItem>
+            <div>
+              <TravelOptionItem :passages=searchResults></TravelOptionItem>
+            </div>
+            <button class="clear-btn" @click="handleClear">Limpar</button>
+            
           </div>
         </div>
       </div>
@@ -146,13 +169,6 @@ onMounted(() => {
   border-radius: 8px;
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
 }
-
-/* @media (max-width: 992px) {
-  .container {
-    margin-left: 0; 
-    width: 100%; 
-  }
-} */
 
 .header {
   display: flex;
@@ -186,18 +202,22 @@ onMounted(() => {
   margin: auto;
   display: flex;
   flex-direction: column;
-  height: 380px;
+  height: 420px;
   justify-content: center;
 
-  /* align-items: center;  */
+}
 
+.left-container-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 360px;
 }
 
 .left-container .title {
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 20px;
 }
 
 .left-container .title i {
@@ -216,7 +236,9 @@ onMounted(() => {
   display: block;
   margin-bottom: 5px;
 }
-
+.select{
+  background-color: white;
+}
 .left-container input[type="text"],
 .left-container input[type="date"] {
   width: 100%;
@@ -226,10 +248,9 @@ onMounted(() => {
   font-size: 16px;
   box-sizing: border-box;
   color: #2d2d2d;
-  background-color: #fafafa
 }
 
-.buscar-btn {
+.search-btn {
   background-color: #007bff;
   color: #fff;
   border: none;
@@ -237,10 +258,24 @@ onMounted(() => {
   border-radius: 4px;
   font-size: 16px;
   cursor: pointer;
+  background-color: #03a7b4;
+  width: 80%;
+  align-self: center;
+}
+
+.clear-btn {
+  background-color: #f2a42f;
+  color: black;
+  border: none;
+  padding: 4px 20px;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+  width: 200px;
+  margin-left: auto;
 }
 
 .right-container {
-  /* background-color: #007bff; */
   color: #2d2d2d;
   padding: 20px;
   border-radius: 4px;
@@ -248,15 +283,45 @@ onMounted(() => {
   margin: auto;
   display: flex;
   flex-direction: column;
-  height: 380px;
+  height: 420px;
   justify-content: center;
   align-items: center;
 }
 
-.right-container-content h3{
+.right-container-content h3 {
   margin-bottom: 16px
-
-
 }
 
+.right-container-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 360px;
+}
+
+.modal {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  background-color: #ffffff;
+  padding-top: 2.5rem;
+  padding-bottom: 1.5rem;
+  padding-left: 5rem;
+  padding-right: 5rem;
+  border-radius: 1rem;
+}
+
+.modal p {
+  font-size: 1.2rem;
+}
+
+.modal button {
+  background-color: #03a8b4;
+  color: #024b55;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  text-transform: none;
+}
 </style>
